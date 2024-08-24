b8d1b4d746555458967b677bd99d3716672aa5ec
park-byeong-gwan <impbk2002@gmail.com>
2024-08-24 17:55:33 +0900

    reconfigure project for universal framework

---
+++ src/MumbleKit/MumbleKit.h
@@ -0,0 +1,32 @@
+//
+//  MumbleKit.h
+//  MumbleKit
+//
+//  Created by 박병관 on 8/24/24.
+//
+
+#ifndef MumbleKit_h
+#define MumbleKit_h
+
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+#endif
+
+#ifdef __APPLE__
+ #include <TargetConditionals.h>
+#endif
+
+#include <MumbleKit/MKAccessControl.h>
+#include <MumbleKit/MKAudio.h>
+#include <MumbleKit/MKCertificate.h>
+#include <MumbleKit/MKChannel.h>
+#include <MumbleKit/MKConnection.h>
+#include <MumbleKit/MKPermission.h>
+#include <MumbleKit/MKServerModel.h>
+#include <MumbleKit/MKServices.h>
+#include <MumbleKit/MKServerPinger.h>
+#include <MumbleKit/MKUser.h>
+#include <MumbleKit/MKVersion.h>
+#include <MumbleKit/MKTextMessage.h>
+
+#endif /* MumbleKit_h */
